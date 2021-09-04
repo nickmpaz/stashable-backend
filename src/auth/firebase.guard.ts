a9beca5fcca14137a9276c50bec3d666eb50dd79
Nick Paz <nicholasmpaz@gmail.com>
2021-09-04 14:19:19 -0700

    console.log

--- src/auth/firebase.guard.ts
+++ src/auth/firebase.guard.ts
@@ -1,9 +1,4 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  Logger,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Injectable()
@@ -12,6 +7,9 @@ export class FirebaseGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const user = await this.authService.validateRequest(request);
+    console.log('FIREBASE GUARD');
+    console.log({ user });
+
     if (user === null) {
       return false;
     }
